67850c160f8819d4aa4111931c2a5f70e78beaa7
nickranger44 <nicholaslueth@gmail.com>
2019-12-03 18:16:58 +0000

    Update getBlog.sh

--- getBlog.sh
+++ getBlog.sh
@@ -30,7 +30,13 @@ echo "Securing MariaDB..."
 mysql_secure_installation
 
 echo "Installing PHP..."
-yum -y install php php-mysql
+yum remove php*
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install yum-utils
+yum-config-manager --enable remi-php70
+yum-config-manager --enable remi-php73
+yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
 
 echo "Restarting HTTP to load new configs..."
 systemctl restart httpd
