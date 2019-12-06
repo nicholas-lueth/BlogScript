bf8cd33abeb35aa8e9b88624518064b1da6bf7e9
nickranger44 <nicholaslueth@gmail.com>
2019-12-06 20:36:32 +0000

    Update getBlog.sh

--- getBlog.sh
+++ getBlog.sh
@@ -1,73 +1,101 @@
 #!/bin/bash
+# Created by Nick Lueth, 12/4/2019.
+# Special thanks to Young Chen for inspiration: https://www.linkedin.com/in/youngchen/
+
+# Make a database user
 echo "Making a DB account. Please remember these for later when dealing with logging in to DB"
 echo "Username:"
 read username
 echo Password:
 read password
+
+# Stored database credentials
 user=${username}
 pass=${password}
+
+# Replacing values in the wp-config.php file
 sed -i 's/*USERNAME*/$user/g' wp-config.php
 sed -i 's/*PASSWORD*/$user/g' wp-config.php
 
-echo "Installing WGET..."
-yum -y install wget
+# Installing wget
+echo "Installing wget..."
+install wget -yq
 
+# Installing httpd
 echo "Installing HTTP..."
-yum -y install httpd
+yum install httpd -yq
 
+# Enabling and starting httpd
 echo "Starting HTTP..." 
 systemctl enable httpd
 systemctl start httpd
 
+# Installing maria db
 echo "Installing Maria DB..."
-yum -y install mariadb-server mariadb
+yum install mariadb-server mariadb -yq
 
+# Enabling and starting 
 echo "Starting MariaDB..."
 systemctl enable mariadb
 systemctl start mariadb
 
+# Installing mysql security features
 echo "Securing MariaDB..."
 mysql_secure_installation
 
+# Installing some new repoitories and installing PHP
 echo "Installing PHP..."
-yum remove php*
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
-yum install yum-utils -y
-yum-config-manager --enable remi-php70 -y
-yum-config-manager --enable remi-php73 -y
-yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -y
+yum remove php* -q
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -yq
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -yq
+yum install yum-utils -yq
+yum-config-manager --enable remi-php70 -yq
+yum-config-manager --enable remi-php73 -yq
+yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -yq
 
+# Restarting httpd
 echo "Restarting HTTP to load new configs..."
 systemctl restart httpd
 
+# Setting firewall rules
 echo "Setting Firewall Rules..."
 firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 
+# Reloading the firewall with the newly configured rules
 echo "Reloading Firewall..."
 firewall-cmd --reload
 
+# Log in to MySQL
 echo "Logging into MySQL..."
 mysql -u root -p
 
-echo "Obtaining WordPress"
-yum -y install php-gd
-systemctl restart httpd
-cd ~
-wget http://wordpress.org/latest.tar.gz
+# Installing php graphing library
+echo "Obtaining WordPress..."
+yum install php-gd -yq
 
-echo "Unzipping Wordpress Install"
+# Restarting httpd
+systemctl restart httpd
+
+# Get and install the latest wordpress package
+wget http://wordpress.org/latest.tar.gz -q
+
+# Unzip the wordpress package
+echo "Unzipping Wordpress Install..."
 tar xzvf latest.tar.gz
 
-echo "Moving Files"
+# Moving the wordpress files to /var/www/html/
+echo "Moving Files..."
 rsync -avP ~/wordpress/ /var/www/html/
 
-echo "Making uploads directory"
+# Creating an uploads directory
+echo "Making uploads directory..."
 mkdir /var/www/html/wp-content/uploads
 
-echo "Changing File Perms"
+# Adjusting file pemissions in /var/www/html/
+echo "Changing File Perms..."
 chown -R apache:apache /var/www/html/*
 
-echo "Making config file"
+# Copying config file over to the wordpress directory
+echo "Sending config file..."
 cp wp-config.php /var/www/html/
